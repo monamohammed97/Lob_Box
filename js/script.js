1b4d13b4723102f7d97a1bcdc144f8a026f0f94b
Mona Mohammed <mona00mona1997@gmail.com>
2023-08-06 19:22:57 +0300

    nav

--- js/script.js
+++ js/script.js
@@ -47,8 +47,10 @@ const button = document.querySelector(".navbar-toggler");
 function checkButtonState() {
   if (button.classList.contains("collapsed")) {
     document.querySelector("header").classList.remove("mobile");
+    document.querySelector("#navbarSupportedContent").classList.add("d-none");
   } else {
     document.querySelector("header").classList.add("mobile");
+    document.querySelector("#navbarSupportedContent").classList.remove("d-none");
   }
 }
 
